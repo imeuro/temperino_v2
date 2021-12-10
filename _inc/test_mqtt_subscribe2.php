6cb9361e2442bee04f271611cfb0b11acd3ae2c8
imeuro <mauro.fioravanzi@gmail.com>
2021-12-10 11:52:22 +0100

    test mosquito client

--- _inc/test_mqtt_subscribe2.php
+++ _inc/test_mqtt_subscribe2.php
@@ -1,21 +1,43 @@
+<h2>Status: inside</h2>
+<time id="in_time_val"></time>
 <?php
-$mqtt_data=[];
-/* Construct a new client instance, passing a client ID of “MyClient” */
-$client = new Mosquitto\Client('MyClient');
-
-/* Set the callback fired when the connection is complete */
+$client = new Mosquitto\Client('temperino');
 $client->onConnect(function($code, $message) use ($client) {
-    /* Subscribe to the broker's $SYS namespace, which shows debugging info */
     $client->subscribe('brtt6/temp', 1);
-    $client->subscribe('brtt6/thermo', 1);
 });
 
-/* Set the callback fired when we receive a message */
 $client->onMessage(function($message) {
     /* Display the message's topic and payload */
     echo $message->topic, "\n", $message->payload, "\n\n";
-    $mqtt_data[$message->topic] = json_decode($message->payload, true);
-    print_r($mqtt_data);
+    $mqtt_temp = json_decode($message->payload, true);
+?>
+    <div class="gauge-container temp-gauge">
+        <canvas width="270" height="250" id="in_TEMPERATURE" class="gauge"></canvas>
+        <span id="in_temp_val" data-type="Temperature" data-scale="°C"><?php echo $mqtt_temp['cur_temp'] ?></span>
+    </div>
+
+    <div class="gauge-container small humi-gauge">
+        <canvas width=135 height=130 id="in_HUMIDITY" class="gauge"></canvas>
+        <span id="in_humi_val" data-type="Humidity" data-scale="%"><?php echo $mqtt_temp['cur_humi'] ?></span>
+    </div>
+<?php
+    $client->exitLoop();
+});
+
+$client->onConnect(function($code, $message) use ($client) {
+    $client->subscribe('brtt6/thermo', 1);
+});
+
+$client->onMessage(function($message) {
+    /* Display the message's topic and payload */
+    echo $message->topic, "\n", $message->payload, "\n\n";
+    $mqtt_thermo = json_decode($message->payload, true);
+?>
+    <div class="otherdata-container small">
+    <button id="program_mode" class="heat-status on"><?php echo $mqtt_thermo['set_prog'] ?></button>
+</div>
+
+<?php
     $client->exitLoop();
 });
 
@@ -26,5 +48,3 @@ $client->connect('meuro.dev', 1883);
 /* Enter the event loop */
 $client->loopForever();
 
-echo 'OUTSIDE:';
-print_r($mqtt_data);
