c77df1b037089b0d977e31387f75ce9cd510379c
imeuro <mauro.fioravanzi@gmail.com>
2021-12-10 02:32:31 +0100

    mqtt data benedetta..

---
+++ _inc/test_mqtt_subscribe2.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Construct a new client instance, passing a client ID of “MyClient” */
+$client = new Mosquitto\Client('MyClient');
+
+/* Set the callback fired when the connection is complete */
+$client->onConnect(function($code, $message) use ($client) {
+    /* Subscribe to the broker's $SYS namespace, which shows debugging info */
+    $client->subscribe('brtt6/thermo', 0);
+});
+
+/* Set the callback fired when we receive a message */
+$client->onMessage(function($message) {
+    /* Display the message's topic and payload */
+    echo $message->topic, "\n", $message->payload, "\n\n";
+});
+
+/* Connect, supplying the host and port. */
+/* If not supplied, they default to localhost and port 1883 */
+$client->connect('meuro.dev', 1883);
+
+/* Enter the event loop */
+$client->loopForever();
